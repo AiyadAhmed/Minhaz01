62231149f376ec7e7ba681f2afc6bdd60a4d8ab5
AiyadAhmed <33205689+AiyadAhmed@users.noreply.github.com>
2017-11-05 20:53:09 -0500

    Update Minhaz01.java
    Addition by Aiyad

--- Minhaz01/src/Minhaz01/Minhaz01.java
+++ Minhaz01/src/Minhaz01/Minhaz01.java
@@ -7,6 +7,12 @@ public class Minhaz01 {
             System.out.println("test 22222");
 		System.out.println("test Borkot");
 		System.out.println("test Borkot - updated 2");
+		int a =2 ;
+		int b = 5;
+		int c = a+b;
+		
+	System.out.println(c);
+
 	}
 
 }
